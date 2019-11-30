3a11a37579b8de23de7dfbee6287f28ff5ed8d68
Ken <57920872+ken-37@users.noreply.github.com>
2019-11-30 00:45:55 -0500

    Update models.py

--- project/models.py
+++ project/models.py
@@ -1,12 +1,37 @@
 from django.db import models
 
 
-class Product(models.Model):
-    p_id = models.AutoField(primary_key=True)
-    price = models.FloatField(default=10.00)
-    p_name = models.CharField(max_length=32)
-    quantity = models.IntegerField(default=500)
-    p_description = models.TextField()
+class Employee(models.Model):
+    DATABASE_ADMINISTRATOR = 'DBA'
+    SALESPERSON = 'Sales'
+    job_choice = [
+        (DATABASE_ADMINISTRATOR, 'Database_Administrator'),
+        (SALESPERSON, 'Salesperson')
+    ]
+
+    e_name = models.CharField(max_length=32)
+    e_email = models.EmailField(unique=True)
+    e_phone = models.IntegerField(max_length=10, unique=True)
+    job_title = models.CharField(max_length=16, choices=job_choice)
+
+
+class EmployeeAddress(models.Model):
+    street = models.CharField(max_length=64)
+    city = models.CharField(max_length=32)
+    state = models.CharField(max_length=2)
+    zip_code = models.IntegerField(max_length=5)
+    e_password = models.CharField(max_length=32)
+    e_address = models.OneToOneField(Employee, on_delete=models.CASCADE)
+
+
+class Customer(models.Model):
+    c_id = models.AutoField(primary_key=True)
+    c_name = models.CharField(max_length=32)
+    c_password = models.CharField(max_length=32)
+    c_email = models.EmailField(unique=True)
+    c_phone = models.IntegerField(max_length=10, unique=True)
+    company = models.CharField(max_length=64)
+    income = models.PositiveIntegerField(null=True)
 
 
 class CustomerAddress(models.Model):
@@ -14,6 +39,15 @@ class CustomerAddress(models.Model):
     city = models.CharField(max_length=32)
     state = models.CharField(max_length=2)
     zip_code = models.IntegerField(max_length=5)
+    c_address = models.OneToOneField(Customer, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    price = models.FloatField(default=10.00)
+    p_name = models.CharField(max_length=32)
+    quantity = models.IntegerField(default=500)
+    sale = models.FloatField(default=1.0)
+    p_description = models.TextField()
 
 
 class OrderHistory(models.Model):
@@ -26,48 +60,11 @@ class OrderHistory(models.Model):
         (DELIVERED, 'Delivered'),
     ]
 
-    o_id = models.AutoField(primary_key=True)
     o_status = models.CharField(max_length=16, choices=status_choice)
     o_time = models.DateTimeField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-
-class Customer(models.Model):
-    c_id = models.AutoField(primary_key=True)
-    c_name = models.CharField(max_length=32)
-    c_password=models.CharField(max_length=32,default="")
-    c_email = models.EmailField(unique=True)
-    c_phone = models.IntegerField(max_length=10, unique=True)
-    company = models.CharField(max_length=64)
-    income = models.PositiveIntegerField(null=True)
-    c_address = models.OneToOneField(CustomerAddress, on_delete=models.CASCADE)
+    order_history = models.OneToOneField(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
-    order_history = models.ForeignKey(OrderHistory, null=True, on_delete=models.CASCADE)
 
-
-    class Employee(models.Model):
-    DATABASE_ADMINISTRATOR = 'DBA'
-    SALESPERSON = 'Sales'
-    job_choice = [
-        (DATABASE_ADMINISTRATOR, 'Database_Administrator'),
-        (SALESPERSON, 'Salesperson')
-    ]
-
-    e_id = models.AutoField(primary_key=True)
-    e_name = models.CharField(max_length=32)
-    e_password=models.CharField(max_length=32,default="")
-    e_email = models.EmailField(unique=True)
-    e_phone = models.IntegerField(max_length=10, unique=True)
-    job_title = models.CharField(max_length=16, choices=job_choice)
-    
-    
-    
-class EmployeeAddress(models.Model):
-    street = models.CharField(max_length=64)
-    city = models.CharField(max_length=32)
-    state = models.CharField(max_length=2)
-    zip_code = models.IntegerField(max_length=5)
-    e_address = models.OneToOneField(Employee, on_delete=models.CASCADE)
 
 class Case(models.Model):
     OPEN = 'O'
@@ -77,14 +74,13 @@ class Case(models.Model):
         (CLOSE, 'Close')
     ]
 
-    ca_id = models.AutoField(primary_key=True)
     summary = models.CharField(max_length=128)
     ca_description = models.TextField(default="the product was damaged after washed.")
     ca_time = models.DateTimeField()
-    ca_status = models.CharField(max_length=10,choices=status_choice)
+    ca_status = models.CharField(max_length=10, choices=status_choice)
     comment = models.TextField(default="We are so sorry for hearing that.")
     customer = models.OneToOneField(Customer, unique=True, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
     employee = models.OneToOneField(Employee, unique=True, on_delete=models.CASCADE)
 
 
